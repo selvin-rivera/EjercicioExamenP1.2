672ddc0cc6586d76bc09daa592a1befb0939ac35
selvin-rivera <nrivera.clements@gmail.com>
2024-08-14 10:17:14 -0600

    ajuste de tamaño de card-img

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ fetch("https://fakestoreapi.com/products")
         let card = `<div class="row row-cols-1 row-cols-md-3 g-4">`
         for (let user of data) {
              card += `<div class="col">
-                    <div class="card">
-                        <img src="${user.image}" class="card-img-top" alt="Usuario">
+                    <div class="card h-100">
+                        <img src="${user.image}" class="card-img-top" alt="user">
                         <div class="card-body">
                             <h5 class="card-title">${user.title}</h5>
                             <ul class="list-group list-group-flush">
-                                <li class="list-group-item"><strong>precio:</strong> ${user. price}</li>
+                                <li class="list-group-item"><strong>Precio:</strong> ${user. price}</li>
                             </ul>
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item"><strong>Descripción:</strong> ${user. description}</li>
